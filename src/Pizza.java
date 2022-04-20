514419799ea4742cdca0777f5ab24563d2683e4a
benj302g <benj302g@stud.kea.dk>
2022-04-20 13:39:32 +0200

    lavet noget på pizza

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,16 @@
 public class Pizza {
+    private int pizzaNumber;
+    private String name;
+    private String description;
+    private double price;
+    private static int numberOfPizzasOnMenu;
+
+    Pizza(String name, String description, double price) {
+        numberOfPizzasOnMenu++;
+        pizzaNumber = numberOfPizzasOnMenu;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
 }
